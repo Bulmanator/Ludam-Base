287d8c6a6cc7dbb01f17262b1451c822028a4628
James Bulman <contact@bulmanator.com>
2021-10-01 20:30:25 +0100

    Fixed issue with looping sounds not looping

--- base/audio.cpp
+++ base/audio.cpp
@@ -35,8 +35,14 @@ function void MixPlayingSounds(Audio_State *state, Asset_Manager *assets, Audio_
 
         sound->samples_played += sample_count;
 
-        b32 is_finished = (sound->samples_played == info->sample_count) && !(sound->flags & PlayingSound_Looped);
-        if (is_finished) { StopSound(state, sound); }
+        b32 is_looped   = (sound->flags & PlayingSound_Looped);
+        b32 is_finished = (sound->samples_played == info->sample_count) && !is_looped;
+        if (is_finished) {
+            StopSound(state, sound);
+        }
+        else if (is_looped) {
+            sound->samples_played = 0;
+        }
 
         sound = next;
     }
